b401b902c3f65021b802569868048acbc563ccab
daddydemir <mehmetcakmaktasi47@gmail.com>
2025-11-02 12:29:25 +0300

    feat: add historical data fetching for empty cache entries
    - Fetch data from Jan 1, 2020 to present for coins with no cache data
    - Handle API's 1-year limit by splitting requests yearly
    - Add rate limiting between API calls to prevent throttling

--- pkg/service/cache_validation_service.go
+++ pkg/service/cache_validation_service.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/daddydemir/crypto/pkg/cache"
 	"github.com/daddydemir/crypto/pkg/remote/coincap"
 	"log/slog"
@@ -51,22 +52,76 @@ func (v *ValidateService) Validate() {
 		}
 
 		if len(array) == 0 {
-			slog.Error("Validate:len(array) == 0", "coin", i.Id)
-			continue
+			slog.Info("Validate:len(array) == 0 - Fetching initial data", "coin", i.Id)
+
+			err := v.fetchHistoricalDataByYear(i.Id, cacheService, client)
+			if err != nil {
+				slog.Error("Validate:fetchHistoricalDataByYear", "coin", i.Id, "error", err)
+				continue
+			}
+
+		} else {
+			_, histories := client.HistoryWithTime(i.Id, array[0].Date.Add(time.Hour*24).UnixNano(), time.Now().UnixNano())
+
+			if len(histories) == 0 {
+				slog.Error("Validate:client.HistoryWithTime", "coin", i.Id, "error", "no new data available")
+				continue
+			}
+
+			err = cacheService.SetList(i.Id, histories, 0)
+			if err != nil {
+				slog.Error("Validate:cacheService.SetList", "coin", i.Id, "error", err)
+				continue
+			}
+
+			slog.Info("Validation Success", "coin", i.Id, "new_records", len(histories))
+			time.Sleep(time.Second * 20)
 		}
-		_, histories := client.HistoryWithTime(i.Id, array[0].Date.Add(time.Hour*24).UnixNano(), time.Now().UnixNano())
+	}
+}
+
+func (v *ValidateService) fetchHistoricalDataByYear(coinId string, cacheService cache.Cache, client coincap.CoinCapClient) error {
+	startDate := time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC)
+	currentTime := time.Now()
+	var allHistories []coincap.History
+
+	for year := 2020; year <= currentTime.Year(); year++ {
+		yearStart := time.Date(year, 1, 1, 0, 0, 0, 0, time.UTC)
+		var yearEnd time.Time
+
+		if year == currentTime.Year() {
+			yearEnd = currentTime
+		} else {
+			yearEnd = time.Date(year, 12, 31, 23, 59, 59, 0, time.UTC)
+		}
+
+		if year == 2020 {
+			yearStart = startDate
+		}
+
+		slog.Info("Fetching data for year", "coin", coinId, "year", year, "start", yearStart.Format("2006-01-02"), "end", yearEnd.Format("2006-01-02"))
+
+		_, histories := client.HistoryWithTime(coinId, yearStart.UnixNano(), yearEnd.UnixNano())
 
 		if len(histories) == 0 {
-			slog.Error("Validate:coincap.HistoryWithTime", "coin", i.Id, "error", "list is empty")
-			continue
+			slog.Warn("No data for year", "coin", coinId, "year", year)
+		} else {
+			allHistories = append(allHistories, histories...)
+			slog.Info("Data fetched for year", "coin", coinId, "year", year, "count", len(histories))
 		}
 
-		err = cacheService.SetList(i.Id, histories, 0)
-		if err != nil {
-			slog.Error("Validate:cacheService.SetList", "error", err)
-			continue
-		}
-		slog.Info("Validation Success", "coin", i.Id)
 		time.Sleep(time.Second * 20)
 	}
+
+	if len(allHistories) == 0 {
+		return fmt.Errorf("no historical data found for coin: %s", coinId)
+	}
+
+	err := cacheService.SetList(coinId, allHistories, 0)
+	if err != nil {
+		return fmt.Errorf("failed to cache historical data: %w", err)
+	}
+
+	slog.Info("All historical data cached successfully", "coin", coinId, "total_count", len(allHistories))
+	return nil
 }
