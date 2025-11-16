07a1810a44960f5c89edd0a54cc7fbd3a2a8631a
daddydemir <mehmetcakmaktasi47@gmail.com>
2025-11-16 20:12:09 +0300

    feat: enhance calculations with price change metrics
    - Add 30-day price change field
    - Add 7-day arithmetic price change calculation
    - Add 30-day arithmetic price change calculation
    - Extend existing calculation logic with new metrics

--- pkg/domain/coin/coin.go
+++ pkg/domain/coin/coin.go
@@ -18,10 +18,16 @@ type ChangeStats struct {
 }
 
 type PriceResult struct {
-	ExchangeID   string  `gorm:"column:exchange_id"`
-	CurrentPrice float64 `gorm:"column:current_price"`
-	DayAgoPrice  float64 `gorm:"column:day_ago_price"`
-	WeekAgoPrice float64 `gorm:"column:week_ago_price"`
-	Change24h    float64 `gorm:"column:change_24h"`
-	Change7d     float64 `gorm:"column:change_7d"`
+	ExchangeID          string  `gorm:"column:exchange_id"`
+	CurrentPrice        float64 `gorm:"column:current_price"`
+	DayAgoPrice         float64 `gorm:"column:day_ago_price"`
+	WeekAgoPrice        float64 `gorm:"column:week_ago_price"`
+	MonthAgoPrice       float64 `gorm:"column:month_ago_price"`
+	AvgWeekAgoPrice     float64 `gorm:"column:avg_7_days_price"`
+	AvgMonthAgoPrice    float64 `gorm:"column:avg_30_days_price"`
+	Change24h           float64 `gorm:"column:change_24h"`
+	Change7d            float64 `gorm:"column:change_7d"`
+	Change30d           float64 `gorm:"column:change_30d"`
+	ChangeArithmetic7d  float64 `gorm:"column:change_arithmetic_7d"`
+	ChangeArithmetic30d float64 `gorm:"column:change_arithmetic_30d"`
 }
